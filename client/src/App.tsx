726d1a236629f640de77f62f0e10fa2a8e6640b6
Samiul Ahmed <58337911+asamiul101@users.noreply.github.com>
2025-12-24 05:20:20 -0500

    added picture for about page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import About from "./components/About";
 import Career from "./components/Career";
+import Projects from "./components/Projects";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="h-screen bg-gradient-to-t from-blue-500 to-white relative">
       <BrowserRouter>
-        <div className="flex absolute inset-0 m-auto rounded-2xl border-solid bg-white items-center justify-center w-[1200px] h-[720px] shadow-2xl">
+        <div className="flex flex-col absolute inset-0 m-auto rounded-2xl border-solid bg-white w-[1200px] h-[720px] shadow-2xl">
           <NavBar />
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/career" element={<Career />} />
-          </Routes>
+          <div className="flex-1 overflow-auto">
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/career" element={<Career />} />
+              <Route path="/projects" element={<Projects />} />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="flex absolute top-0 w-full justify-between py-5 px-6">
-      <div>Portfolio</div>
-      <div className="flex gap-4">
-        <Link to="/about">About</Link>
+    <div className="flex justify-between py-5 px-10">
+      <div className="italic pl-10 text-3xl text-blue-500">Portfolio</div>
+      <div className="flex gap-6">
+        <Link to="/">About</Link>
         <Link to="/career">Career</Link>
+        <Link to="/projects">Projects</Link>
       </div>
     </div>
   );
