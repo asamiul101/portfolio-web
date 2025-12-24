1b671a821a8c6d166d73be8f7b24bef5ca6ec8c1
Samiul Ahmed <58337911+asamiul101@users.noreply.github.com>
2025-12-24 04:29:33 -0500

    added navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Career from "./components/Career";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">Hello, World!</h1>
+    <div className="h-screen bg-gradient-to-t from-blue-500 to-white relative">
+      <BrowserRouter>
+        <div className="flex absolute inset-0 m-auto rounded-2xl border-solid bg-white items-center justify-center w-[1200px] h-[720px] shadow-2xl">
+          <NavBar />
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/career" element={<Career />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
